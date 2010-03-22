cab2385c4c3ecca6cbe6b02ee96320d0faa1f9a6
AD7six <andydawson76@yahoo.co.uk>
2010-03-22 22:09:18 +0100

    updating control file, adding readme stub

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mi Asset. Intelligent css and js management
+
+Mi Asset provides automatic css and js minimization and concatenation to both reduce http requests and overall page weight (and therefore load times)
--- config/control.php
+++ config/control.php
@@ -1,9 +1,8 @@
 #<?php die() ?>
 Package: mi_asset
-Source: http://svn.assembla.com/svn/mi/branches/mi_asset
+Source: git://github.com/AD7six/mi_asset.git
 Type: plugin
 Section: assets
 Depends: mi
 Description: Compression and handling functionality for css/js files
- An administrative plugin for Access Control List (ACL) data
-Bugs: http://thechaw.com/mi/tickets/add
+Bugs: http://github.com/AD7six/mi_asset/issues
